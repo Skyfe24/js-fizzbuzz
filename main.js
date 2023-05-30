6ef29f108b9018e8983960710fc95d939c8213fc
Skyfe24 <fabio.erase@gmail.com>
2023-05-30 15:33:23 +0200

    excercise completed

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 console.log('JS OK');
+const container = document.getElementById("big-red");
 
-for (let i=1;  i<=100; i++) {
-    console.log('ALL THE NUMBERS ARE ', i );  
+
+for(let i=1;  i<=100; i++) {
+    
+    if (i % 3 === 0){ 
+    console.log('fizz');
+}
+    else if (i % 5 === 0) {
+    console.log('buzz');
+}
+    if (i % 3 === 0 && i % 5 === 0) {
+    console.log('fizzbuzz');
+}
+    else{
+    console.log(i);
+}
 }
