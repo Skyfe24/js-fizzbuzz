5362dcbf710f2be4edcecb2268a1252ab6854c50
Skyfe24 <fabio.erase@gmail.com>
2023-05-30 16:51:48 +0200

    BONUS 1 BONUS 2 and Frog

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 console.log("JS OK");
-const container = document.getElementById("big-red");
+let container = document.getElementById("big-red");
+let result = document.getElementById("result");
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
+    container.innerHTML +=`<div class="card1"> fizz </div>`;
     console.log("fizz");
   } else if (i % 5 === 0) {
+    container.innerHTML +=`<div class="card2"> buzz </div>`;
     console.log("buzz");
   }
   if (i % 3 === 0 && i % 5 === 0) {
+    container.innerHTML +=`<div class="card3"> fizzbuzz </div>`;
     console.log("fizzbuzz");
   } else {
+    container.innerHTML +=`<div class="card4"> ${i} </div>`;
     console.log(i);
   }
 }
