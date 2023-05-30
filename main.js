5f672d4c2cde7dccde17aa12d56bde4374813f7a
Skyfe24 <fabio.erase@gmail.com>
2023-05-30 15:35:04 +0200

    complete

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-console.log('JS OK');
+console.log("JS OK");
 const container = document.getElementById("big-red");
 
-
-for(let i=1;  i<=100; i++) {
-    
-    if (i % 3 === 0){ 
-    console.log('fizz');
-}
-    else if (i % 5 === 0) {
-    console.log('buzz');
-}
-    if (i % 3 === 0 && i % 5 === 0) {
-    console.log('fizzbuzz');
-}
-    else{
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  }
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else {
     console.log(i);
-}
+  }
 }
